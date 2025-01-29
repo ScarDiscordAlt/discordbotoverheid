ac9f9edb600374660d9b0d87f47eed559b82ac88
ScarDiscordAlt <135650542+ScarDiscordAlt@users.noreply.github.com>
2025-01-29 22:20:51 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Voeg eventuele interactieve functies toe indien nodig
+document.querySelector('.btn').addEventListener('click', function() {
+    alert('Bedankt voor het toevoegen van onze bot!');
+});
